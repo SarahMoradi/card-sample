0cb3cb116e2ba53fa207b6c38e6694c093c15470
Moradi <s.moradi@asiatech.cloud>
2022-09-05 15:56:56 +0430

    completed right section of sample card

--- src/components/right-card/RightCard.js
+++ src/components/right-card/RightCard.js
@@ -1,8 +1,17 @@
 import styles from "./rightcard.module.css";
 
 const RightCard = () => {
-  return <div className={styles.right_info_container}>
+  return (
+    <div className={styles.right_info_container}>
       <h1 className={styles.header_section}>Jane Doe</h1>
-  </div>;
+      <p className={styles.right_content}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat
+      </p>
+      <p className={styles.more_details}>Mouse over the card for more info</p>
+    </div>
+  );
 };
 export default RightCard;
