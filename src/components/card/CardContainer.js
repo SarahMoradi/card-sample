fcbc55fb714ecd12e19642a55177ec17aa27475d
Moradi <s.moradi@asiatech.cloud>
2022-09-05 12:25:48 +0430

    completed left section in card-sample

--- src/components/card/CardContainer.js
+++ src/components/card/CardContainer.js
@@ -3,7 +3,19 @@ const CardContainer = () => {
   return (
     <div className={styles.card_container}>
       <div className={styles.card}>
-        <div className={styles.left_section}>left</div>
+        <div className={styles.left_section}>
+          <div>
+            <div className={styles.level_container}>LEVEL 13</div>
+            <div className={styles.image_container}>
+              <img
+                src={process.env.PUBLIC_URL + "logo.png"}
+                alt="logo"
+                width={80}
+              />
+            </div>
+            <div className={styles.points_container}>5,312 POINTS</div>
+          </div>
+        </div>
         <div className={styles.right_section}>right</div>
       </div>
     </div>
